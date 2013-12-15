fc41580d45523f97327e8e4cd2b50c64865ac884
Josef Patoprsty <seppi@josefnpat.com>
2013-12-15 00:24:14 -0500

    Working...

--- src/gamestate_game.lua
+++ src/gamestate_game.lua
@@ -5,9 +5,9 @@ function gamestate_game.init()
   -- Move Frame
 
   gamestate_game.frame_move = loveframes.Create("frame")
-  gamestate_game.frame_move:SetSize(320,2.5*32)
+  gamestate_game.frame_move:SetSize(64*4+32,2.5*32)
   gamestate_game.frame_move:ShowCloseButton(false)
-  gamestate_game.frame_move:SetName("Move")
+  gamestate_game.frame_move:SetName("Warp Destination")
 
   gamestate_game.move_x = loveframes.Create("numberbox",gamestate_game.frame_move)
   gamestate_game.move_x:SetPos(16+(64)*(1-1),32)
@@ -27,7 +27,7 @@ function gamestate_game.init()
   gamestate_game.move = loveframes.Create("button",gamestate_game.frame_move)
   gamestate_game.move:SetSize(64,32)
   gamestate_game.move:SetPos(16+(62+2)*(4-1),32)
-  gamestate_game.move:SetText("Travel")
+  gamestate_game.move:SetText("Warp")
   gamestate_game.move.OnClick = function(object)
     com.enqueue_request(
       {func="move",
@@ -47,7 +47,7 @@ function gamestate_game.init()
   gamestate_game.msgbox = {}
   gamestate_game.msgbox.x = 16
   gamestate_game.msgbox.y = 32
-  gamestate_game.msgbox.w = 640
+  gamestate_game.msgbox.w = love.graphics.getWidth()/2
   gamestate_game.msgbox.h = 160
 
   gamestate_game.frame_msgbox = loveframes.Create("frame")
@@ -56,20 +56,73 @@ function gamestate_game.init()
   gamestate_game.frame_msgbox:ShowCloseButton(false)
   gamestate_game.frame_msgbox:SetPos(0,love.graphics.getHeight()-gamestate_game.msgbox.h)
 
-  gamestate_game.msgbox_list = loveframes.Create("list", gamestate_game.frame_msgbox)
+  gamestate_game.msgbox_list = loveframes.Create("list",gamestate_game.frame_msgbox)
   gamestate_game.msgbox_list:SetPos(gamestate_game.msgbox.x,gamestate_game.msgbox.y)
   gamestate_game.msgbox_list:SetSize(
     gamestate_game.msgbox.w-gamestate_game.msgbox.x*2,
-    gamestate_game.msgbox.h-gamestate_game.msgbox.y-16
+    gamestate_game.msgbox.h-gamestate_game.msgbox.y-16-32
   )
 
-  gamestate_game.msgbox_text  = loveframes.Create("text", gamestate_game.msgbox_list)
+  gamestate_game.msgbox_text = loveframes.Create("text", gamestate_game.msgbox_list)
   gamestate_game.msgbox_text:SetPos(16,32)
   gamestate_game.msgbox_text:SetSize(
     gamestate_game.msgbox.w-gamestate_game.msgbox.x*2,
     gamestate_game.msgbox.h-gamestate_game.msgbox.y-16
   )
-  gamestate_game.msgbox_add("Welcome space travler.")
+  gamestate_game.msgbox_add("Welcome space travler. A friendly reminder: You only get one life.")
+
+  gamestate_game.msgbox_chatin = loveframes.Create("textinput",gamestate_game.frame_msgbox)
+  gamestate_game.msgbox_chatin:SetLimit(128)
+  gamestate_game.msgbox_chatin:SetPos(
+    gamestate_game.msgbox.x,
+    gamestate_game.msgbox.h - gamestate_game.msgbox.y - 8)
+
+  gamestate_game.msgbox_chatin:SetSize(
+    gamestate_game.msgbox.w-gamestate_game.msgbox.x*2 -64,
+    32
+  )
+
+  gamestate_game.msgbox_chatsend = loveframes.Create("button",gamestate_game.frame_msgbox)
+  gamestate_game.msgbox_chatsend:SetPos(
+    gamestate_game.msgbox.w - gamestate_game.msgbox.x - 64,
+    gamestate_game.msgbox.h - gamestate_game.msgbox.y - 8)
+
+  gamestate_game.msgbox_chatsend:SetSize(64, 32)
+  gamestate_game.msgbox_chatsend:SetText("Send")
+  gamestate_game.msgbox_chatsend.OnClick = function(object)
+     com.enqueue_request(
+      {func="sendchat",
+       args = {
+         msg = gamestate_game.msgbox_chatin:GetValue()
+        }
+      },
+      requests.chatsend)
+  end
+
+
+  -- Player list
+
+  gamestate_game.playerlist = {}
+  gamestate_game.playerlist.x = 16
+  gamestate_game.playerlist.y = 32
+  gamestate_game.playerlist.w = 160
+  gamestate_game.playerlist.h = love.graphics.getHeight() / 2
+
+  gamestate_game.frame_playerlist = loveframes.Create("frame")
+  gamestate_game.frame_playerlist:SetName("Playerlist")
+  gamestate_game.frame_playerlist:SetSize(gamestate_game.playerlist.w,gamestate_game.playerlist.h)
+  gamestate_game.frame_playerlist:ShowCloseButton(false)
+  gamestate_game.frame_playerlist:SetPos(
+    love.graphics.getWidth()-gamestate_game.playerlist.w,
+    love.graphics.getHeight()-gamestate_game.playerlist.h)
+
+  gamestate_game.playerlist_list = loveframes.Create("list", gamestate_game.frame_playerlist)
+  gamestate_game.playerlist_list:SetPos(gamestate_game.playerlist.x,gamestate_game.playerlist.y)
+  gamestate_game.playerlist_list:SetSize(
+    gamestate_game.playerlist.w-gamestate_game.playerlist.x*2,
+    gamestate_game.playerlist.h-gamestate_game.playerlist.y-16
+  )
+ 
 
 end
 
@@ -101,15 +154,18 @@ function gamestate_game.update(self,dt)
       if not gamestate_game.inwarp then
         gamestate_game.inwarp = true
         -- CONFIRMED IN WARP TRIGGERS
+        gamestate_game.move:SetEnabled(false)
       end
     else
       if gamestate_game.inwarp then
         gamestate_game.inwarp = false
         -- CONFIRMED OUT OF WARP TRIGGERS
         gamestate_game.msgbox_add("Arriving at destination.")
+        gamestate_game.move:SetEnabled(true)
       end
     end
   end
+
 end
 
 function gamestate_game.draw(self)
@@ -117,14 +173,10 @@ function gamestate_game.draw(self)
     "WELCOME TO THE PRE-PRE-PRE-DARE-PROTOTYPE-ALPHA\n"..
     "USER "..settings.data.username.."\n"..
     "Your current position is ("..userdata.x..","..userdata.y..","..userdata.z..")\n"..
+    "Your ap: "..userdata.ap.."/"..userdata.ap_max.."\n"..
     "Your speed is: "..userdata.speed.." m/s\n"..
-    "Your warp eta is: "..userdata.warp_eta.." (0 means not in warp.)\n"..
+    ( (userdata.warp_eta>0) and ("ETA "..round(userdata.warp_eta-ctime).." seconds") or ("Not in warp." ) ).."\n"..
     "",100,100,600,"center")
-  if usersdata then
-    for i,v in pairs(usersdata) do
-      love.graphics.printf(v.name .. " ("..v.x..","..v.y..","..v.z..")",650,100+12*(i-1),100,"center")
-    end
-  end
 end
 
 return gamestate_game
--- src/requests.lua
+++ src/requests.lua
@@ -2,12 +2,108 @@ requests = {}
 
 function requests.getuser(response)
   userdata = response.ret
+
+  if userdata and 
+    gamestate_game.move_x and
+    gamestate_game.move_y and
+    gamestate_game.move_z then
+
+    gamestate_game.move_x:SetMin(userdata.x-userdata.warp_range)
+    gamestate_game.move_x:SetMax(userdata.x+userdata.warp_range)
+
+    gamestate_game.move_y:SetMin(userdata.y-userdata.warp_range)
+    gamestate_game.move_y:SetMax(userdata.y+userdata.warp_range)
+
+    gamestate_game.move_z:SetMin(userdata.z-userdata.warp_range)
+    gamestate_game.move_z:SetMax(userdata.z+userdata.warp_range)
+
+  end
+
   Gamestate.switch(states['game'])
 end
 
 function requests.getusers(response)
-  usersdata = response.ret
-  Gamestate.switch(states['game'])
+
+  local unique_playersr = {}
+  if response.ret then
+    for i,v in pairs(response.ret) do
+      unique_playersr[v.name.."_"..v.x.."_"..v.y.."_"..v.z] = true
+    end
+  end
+
+  local unique_playersu = {}
+  if usersdata then
+    for i,v in pairs(usersdata) do
+      unique_playersu[v.name.."_"..v.x.."_"..v.y.."_"..v.z] = true
+    end
+  end
+
+  local update_list = false
+
+  for i,v in pairs(unique_playersu) do
+    if not unique_playersr[i] then
+      update_list = true
+      break
+    end
+  end
+
+  if not update_list then
+    for i,v in pairs(unique_playersr) do
+      if not unique_playersu[i] then
+        update_list = true
+        break
+      end
+    end
+  end
+
+  if update_list then
+
+    usersdata = response.ret
+
+    gamestate_game.playerlist_list:Clear()
+    if usersdata then
+      for _,v in pairs(usersdata) do
+        local button = loveframes.Create("button")
+        button:SetSize(100, 25)
+        button:SetText(v.name.." ("..v.x..","..v.y..","..v.z..")")
+        if v.x ~= userdata.x or 
+          v.y ~= userdata.y or 
+          v.z ~= userdata.z then
+
+          button.targetx = v.x
+          button.targety = v.y
+          button.targetz = v.z
+
+          button.OnClick = function(object)
+            gamestate_game.move_x:SetValue(object.targetx)
+            gamestate_game.move_y:SetValue(object.targety)
+            gamestate_game.move_z:SetValue(object.targetz)
+            gamestate_game.msgbox_add("Updated warp destination to target.")
+          end
+
+        else
+
+          button.target = v.name 
+          button.OnClick = function(object)
+            com.enqueue_request(
+              {
+              func="attack",
+                args = {
+                  target = object.target
+                }
+              },
+              requests.move
+            )
+          end
+
+        end
+
+        gamestate_game.playerlist_list:AddItem(button)
+      end
+    end
+   
+  end
+ 
 end
 
 function requests.move(response)
@@ -16,7 +112,15 @@ function requests.move(response)
       gamestate_game.msgbox_add("Error: "..v)
     end
   else
-    gamestate_game.msgbox_add("Warping to ("..userdata.x..","..userdata.y..","..userdata.z..")")
+    gamestate_game.msgbox_add("Warping.")
+  end
+end
+
+function requests.chatsend(response)
+  if response.error then
+    for _,v in pairs(response.error) do
+      gamestate_game.msgbox_add("Error: "..v)
+    end
   end
 end
 
