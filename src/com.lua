65163c0f528e5107eecae2e417d28b59a8466493
Josef Patoprsty <seppi@josefnpat.com>
2013-12-15 00:38:22 -0500

    Adding client side _GET to _POST conversion

--- src/com.lua
+++ src/com.lua
@@ -80,22 +80,4 @@ function com.update()
   end
 end
 
-function com.blocking_request(object)
-  print("STOP USING THIS.")
-  object.username = settings.data.username
-  local j = json.encode(object)
-  local sha1obj = hashlib.sha1()
-  sha1obj:process(j..settings.data.secure_key)
-  local hash = sha1obj:finish()
-  local payload = hash..j
-  local response = http.request(server_base_url.."server.php?i="..payload)
-  local success,ret = pcall(function() return json.decode(response) end)
-  if success then
-    return ret
-  else
-    print(response)
-    return {error={"could not decode json."}}
-  end
-end
-
 return com 
--- src/com_thread.lua
+++ src/com_thread.lua
@@ -9,6 +9,10 @@ while true do
   thisThread:set("ready",true)
   local payload = thisThread:demand('payload')
   thisThread:set("ready",false)
-  local response = http.request(server_base_url.."server.php?i="..payload)
+
+  local response = http.request(
+    server_base_url.."server.php",
+    "i="..payload)
+
   thisThread:set("response",response or "fail")
 end
